d0c6041accf6b189fbc24b6b45e0f9317135592f
RPEA <richard.peach@softwareag.com>
2020-05-02 21:36:16 +0100

    Adding required webpack config

---
+++ runtime-widget-webpack.config.js
@@ -0,0 +1,25 @@
+const URLImportPlugin  = require("webpack-external-import/webpack");
+
+module.exports = function config() {
+    return {
+        plugins: [
+            new URLImportPlugin ({
+                manifestName: "CumulocityApplicationWithRuntimeWidgets",
+                provideExternals: {
+                    "@angular/animations": "AngularAnimations",
+                    "@angular/common": "AngularCommon",
+                    "@angular/common/http": "AngularCommonHttp",
+                    "@angular/cdk": "AngularCdk",
+                    "@angular/core": "AngularCore",
+                    "@angular/forms": "AngularForms",
+                    "@angular/http": "AngularHttp",
+                    "@angular/platform-browser": "AngularPlatformBrowser",
+                    "@angular/platform-browser/animations": "AngularPlatformBrowserAnimations",
+                    "@angular/router": "AngularRouter",
+                    "@c8y/client": "C8yClient",
+                    "@c8y/ngx-components": "C8yNgxComponents"
+                }
+            })
+        ]
+    }
+};
